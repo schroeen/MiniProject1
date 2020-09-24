68b05716ea1e22b91967968fcf54b4c7db38a977
schroeen <schroeen@mail.uc.edu>
2020-09-24 02:03:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,55 @@
+/*
+Mini Project 1
+Group #_: Evan Schroeder, Damandeep Kaur, Jacob Knapke 
+Date: 9/28/20
+*/
+import java.util.Random;
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the Guessing Game!");
+    Scanner s = new Scanner(System.in);
+    
+    Random r = new Random();
+
+    int num;
+    int guess;
+    int tries=0;
+    System.out.println("Please enter any positive whole number: ");
+     num = s.nextInt();
+
+    int randomNum = r.nextInt(num)+1;
+
+    System.out.println("A random number to guess has been generated");
+    playGame(randomNum, num);
+    guess = s.nextInt();
+    
+    while (guess != randomNum) {
+      
+      if (guess>randomNum)
+      {
+        System.out.println("Guess lower!");
+        System.out.println("Enter your new guess: ");
+      }
+      else if (guess<randomNum) 
+      {
+        System.out.println("Guess higher!");
+        System.out.println("Enter your new guess: ");
+      }
+      guess = s.nextInt();
+      tries++;
+    }
+    System.out.println("Great, you win! It took you "+ tries +" tries"); 
+  }
+
+
+  static void playGame(int theRN, int theNum)
+  {
+    System.out.println("Please guess a number between 0 and "+theNum);
+    
+  }
+  
+}
